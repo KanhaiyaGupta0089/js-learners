699a45d494f18fc14f5e17d0df48beef986c9f10
Kanhaiya Gupta <116626934+KanhaiyaGupta0089@users.noreply.github.com>
2024-04-05 15:14:24 +0000

    git config file

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all js code as newer version of js
+
+//alert(3+3)//gives error in js but not in browser because we are using nodejs , not browser
+
+//NUMBER=> range -> 2^53
+//bigint
+//string =>""
+//boolean -> true or false
+//null -> standalone values which means empty. 
+//undefined -> means variable is declared but not initialized then it give undefined.
+//Symbol ->used for unique identification/
+//object
+// Type of(null)=object
+// Type of(undefined)=undefined
